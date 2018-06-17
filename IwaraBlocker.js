febaccb6b81a6df4c4060464fa6dfb2d0e490f8c
sam831333 <40332939+sam831333@users.noreply.github.com>
2018-06-17 19:37:25 +1000

    Add files via upload

--- IwaraBlocker.js
+++ IwaraBlocker.js
@@ -6,13 +6,12 @@
             filepath = browser.extension.getURL("blocklist.txt");
             
         var text = readText(filepath).split(/[\r\n]+/);
-        
         for (var i = 0, maxi = allElements.length; i < maxi; i++) {
             for (var j = 0, maxj = text.length; j < maxj; j++) {
                 var re = new RegExp(text[j].trim(), "ig");
                 var matches = re.exec(allElements[i].innerHTML);
                 if (matches) {
-                    // console.log("Blocking " + re);
+                    console.log("Blocking " + text[j].trim());
                     allElements[i].style.display = "none";
                 }
             }
@@ -21,8 +20,8 @@
     
     function readText(filename) {
         var rawFile = new XMLHttpRequest(), text;
-        rawFile.open("get", file, false);
-        rawFile.onload = function() {
+        rawFile.open("get", filename, false);
+        rawFile.onreadystatechange = function() {
             if (rawFile.readyState === 4) {
                 if (rawFile.status === 200 || rawFile.status == 0) text = rawFile.responseText;
             }
