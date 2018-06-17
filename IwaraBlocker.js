c52c1c271c13ac01d03d215ac6e671becb36ffac
sam831333 <40332939+sam831333@users.noreply.github.com>
2018-06-17 18:40:51 +1000

    Initial implementation

---
+++ IwaraBlocker.js
@@ -0,0 +1,33 @@
+(function() {
+    hideCancer();
+    
+    function hideCancer() {
+        var allElements = document.querySelectorAll("div.views-column"),
+            filepath = browser.extension.getURL("blocklist.txt");
+            
+        var text = readText(filepath).split(/[\r\n]+/);
+        
+        for (var i = 0, maxi = allElements.length; i < maxi; i++) {
+            for (var j = 0, maxj = text.length; j < maxj; j++) {
+                var re = new RegExp(text[j].trim(), "ig");
+                var matches = re.exec(allElements[i].innerHTML);
+                if (matches) {
+                    // console.log("Blocking " + re);
+                    allElements[i].style.display = "none";
+                }
+            }
+        }
+    }
+    
+    function readText(filename) {
+        var rawFile = new XMLHttpRequest(), text;
+        rawFile.open("get", file, false);
+        rawFile.onload = function() {
+            if (rawFile.readyState === 4) {
+                if (rawFile.status === 200 || rawFile.status == 0) text = rawFile.responseText;
+            }
+        }
+        rawFile.send(null);
+        return text;
+    }
+}());
